df3d7e0899fcdf8c18831ab851322d27d301589f
Narendra Kumar <95516178+NarendraKumarMadireddy@users.noreply.github.com>
2024-05-05 03:16:41 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,57 @@
+import random
+import string
+
+def generatePassword(pwlength):
+    alphabet = string.ascii_lowercase
+    special_chars = string.punctuation
+
+    passwords = [] 
+
+    for length in pwlength:
+        password = "" 
+        num_special_chars = 0
+        for _ in range(length):
+            if num_special_chars < 2 and random.random() < 0.5:
+                password += random.choice(special_chars)
+                num_special_chars += 1
+            else:
+                password += random.choice(alphabet)
+        
+        password = replaceWithNumber(password)
+        password = replaceWithUppercaseLetter(password)
+        
+        passwords.append(password) 
+    
+    return passwords
+
+def replaceWithNumber(pword):
+    for _ in range(random.randrange(1, 3)):
+        replace_index = random.randrange(len(pword)//2)
+        pword = pword[:replace_index] + str(random.randrange(10)) + pword[replace_index+1:]
+    return pword
+
+def replaceWithUppercaseLetter(pword):
+    for _ in range(random.randrange(1, 3)):
+        replace_index = random.randrange(len(pword)//2, len(pword))
+        pword = pword[:replace_index] + pword[replace_index].upper() + pword[replace_index+1:]
+    return pword
+
+def main():
+    numPasswords = int(input("How many passwords do you want to generate? "))
+    print("Generating " + str(numPasswords) + " passwords")
+    
+    passwordLengths = []
+    print("Minimum length of password should be 3")
+    
+    for i in range(numPasswords):
+        length = int(input("Enter the length of Password #" + str(i+1) + " "))
+        if length < 3:
+            length = 3
+        passwordLengths.append(length)
+    
+    Password = generatePassword(passwordLengths)
+
+    for i in range(numPasswords):
+        print("Password #" + str(i+1) + " = " + Password[i])
+
+main()
